4acbdaa9600a7a406c176d4f34cb978ed1e5eb87
zaxwebs <zifidigital@gmail.com>
2023-05-04 03:17:49 +0530

    basics

---
+++ src/lib/data.js
@@ -0,0 +1,39 @@
+export const settings = {
+	dailyHours: 12,
+}
+
+export const groups = [
+	{
+		id: 1,
+		name: 'Group A',
+		hourlyWage: 100,
+	},
+	{
+		id: 2,
+		name: 'Group B',
+		hourlyWage: 80,
+	},
+]
+
+export const employees = [
+	{
+		id: 1,
+		name: 'Employee A',
+		groupId: 1,
+	},
+	{
+		id: 2,
+		name: 'Employee C',
+		groupId: 1,
+	},
+	{
+		id: 3,
+		name: 'Employee D',
+		groupId: 1,
+	},
+	{
+		id: 4,
+		name: 'Employee E',
+		groupId: 2,
+	},
+]
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,57 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Heading, P, Span } from 'flowbite-svelte'
+	import {
+		Table,
+		TableBody,
+		TableBodyCell,
+		TableBodyRow,
+		TableHead,
+		TableHeadCell,
+		Input,
+	} from 'flowbite-svelte'
+	import { groups, employees, settings } from '$lib/data'
+
+	const employeesByGroup = (id) => {
+		return employees.filter((employee) => employee.groupId === id)
+	}
+</script>
+
+<div class="container mx-auto p-4">
+	{#each groups as group (group.id)}
+		<div class="mb-6">
+			<Heading tag="h4" class="mb-4">{group.name}</Heading>
+			<Table hoverable={true}>
+				<TableHead>
+					<TableHeadCell>Name</TableHeadCell>
+					<TableHeadCell>Standard Days</TableHeadCell>
+					<TableHeadCell>Dock Holidays</TableHeadCell>
+					<TableHeadCell>Extra Days</TableHeadCell>
+					<TableHeadCell>Total Days</TableHeadCell>
+					<TableHeadCell>Overtime Hours</TableHeadCell>
+				</TableHead>
+				<TableBody>
+					{#each employeesByGroup(group.id) as employee (employee.id)}
+						<TableBodyRow>
+							<TableBodyCell>{employee.name}</TableBodyCell>
+							<TableBodyCell>
+								<Input type="number" />
+							</TableBodyCell>
+							<TableBodyCell>
+								<Input type="number" />
+							</TableBodyCell>
+							<TableBodyCell>
+								<Input type="number" />
+							</TableBodyCell>
+							<TableBodyCell>
+								<Input type="number" />
+							</TableBodyCell>
+							<TableBodyCell>
+								<Input type="number" />
+							</TableBodyCell>
+						</TableBodyRow>
+					{/each}
+				</TableBody>
+			</Table>
+		</div>
+	{/each}
+</div>
