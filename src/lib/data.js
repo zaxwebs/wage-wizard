86043cea86b5109f19c0e813b0fc8f9a72943f0a
zaxwebs <zifidigital@gmail.com>
2023-05-04 04:58:40 +0530

    mvp done

--- src/lib/data.js
+++ src/lib/data.js
@@ -5,12 +5,12 @@ export const settings = {
 export const groups = [
 	{
 		id: 1,
-		name: 'Group A',
-		hourlyWage: 100,
+		name: 'Office Staff',
+		hourlyWage: 40,
 	},
 	{
 		id: 2,
-		name: 'Group B',
+		name: 'Crew / A',
 		hourlyWage: 80,
 	},
 ]
@@ -20,20 +20,44 @@ export const employees = [
 		id: 1,
 		name: 'Employee A',
 		groupId: 1,
+		days: {
+			standard: 0,
+			dh: 0,
+			ed: 0,
+		},
+		overtime: 0,
 	},
 	{
 		id: 2,
 		name: 'Employee C',
 		groupId: 1,
+		days: {
+			standard: 0,
+			dh: 0,
+			ed: 0,
+		},
+		overtime: 0,
 	},
 	{
 		id: 3,
 		name: 'Employee D',
 		groupId: 1,
+		days: {
+			standard: 0,
+			dh: 0,
+			ed: 0,
+		},
+		overtime: 0,
 	},
 	{
 		id: 4,
 		name: 'Employee E',
 		groupId: 2,
+		days: {
+			standard: 0,
+			dh: 0,
+			ed: 0,
+		},
+		overtime: 0,
 	},
 ]
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,17 +9,33 @@
 		TableHeadCell,
 		Input,
 	} from 'flowbite-svelte'
+
 	import { groups, employees, settings } from '$lib/data'
 
 	const employeesByGroup = (id) => {
 		return employees.filter((employee) => employee.groupId === id)
+	}
+
+	const sumDays = (daysObject) => {
+		return parseInt(daysObject.standard) + parseInt(daysObject.dh) + parseInt(daysObject.ed)
+	}
+
+	const daysToHours = (days, hoursPerDay) => {
+		return days * hoursPerDay
+	}
+
+	const calculateOvertime = (days, dailyHours, overtimeHours) => {
+		return days * dailyHours + hours
 	}
 </script>
 
 <div class="container mx-auto p-4">
 	{#each groups as group (group.id)}
 		<div class="mb-6">
-			<Heading tag="h4" class="mb-4">{group.name}</Heading>
+			<div class="flex">
+				<Heading tag="h4" class="mb-4">{group.name}</Heading>
+				<div>${group.hourlyWage}/hr</div>
+			</div>
 			<Table hoverable={true}>
 				<TableHead>
 					<TableHeadCell>Name</TableHeadCell>
@@ -28,26 +44,55 @@
 					<TableHeadCell>Extra Days</TableHeadCell>
 					<TableHeadCell>Total Days</TableHeadCell>
 					<TableHeadCell>Overtime Hours</TableHeadCell>
+					<TableHeadCell>Total Hours</TableHeadCell>
+					<TableHeadCell>Payout</TableHeadCell>
 				</TableHead>
 				<TableBody>
 					{#each employeesByGroup(group.id) as employee (employee.id)}
 						<TableBodyRow>
 							<TableBodyCell>{employee.name}</TableBodyCell>
 							<TableBodyCell>
-								<Input type="number" />
+								<Input
+									type="number"
+									min="0"
+									step="1"
+									bind:value={employee.days.standard}
+								/>
 							</TableBodyCell>
 							<TableBodyCell>
-								<Input type="number" />
+								<Input
+									type="number"
+									min="0"
+									step="1"
+									bind:value={employee.days.dh}
+								/>
 							</TableBodyCell>
 							<TableBodyCell>
-								<Input type="number" />
+								<Input
+									type="number"
+									min="0"
+									step="1"
+									bind:value={employee.days.ed}
+								/>
 							</TableBodyCell>
+							<TableBodyCell>{sumDays(employee.days)}</TableBodyCell>
 							<TableBodyCell>
-								<Input type="number" />
+								<Input
+									type="number"
+									min="0"
+									step="1"
+									bind:value={employee.overtime}
+								/>
 							</TableBodyCell>
-							<TableBodyCell>
-								<Input type="number" />
-							</TableBodyCell>
+							<TableBodyCell
+								>{daysToHours(sumDays(employee.days), settings.dailyHours) +
+									parseInt(employee.overtime)}</TableBodyCell
+							>
+							<TableBodyCell
+								>{(daysToHours(sumDays(employee.days), settings.dailyHours) +
+									parseInt(employee.overtime)) *
+									group.hourlyWage}</TableBodyCell
+							>
 						</TableBodyRow>
 					{/each}
 				</TableBody>
