49e8ad175deb19f972fa2c241a8cbbc47a8ed49d
zaxwebs <zifidigital@gmail.com>
2023-05-04 07:46:51 +0530

    updated icon

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -6,7 +6,7 @@
 <Sidebar>
 	<SidebarWrapper>
 		<SidebarGroup>
-			<SidebarItem label="Dashboard">
+			<SidebarItem label="Payout Calculator">
 				<svelte:fragment slot="icon">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
@@ -84,7 +84,7 @@
 					>
 				</svelte:fragment>
 			</SidebarItem>
-			<SidebarItem label="Settings" {spanClass}>
+			<SidebarItem label="Settings">
 				<svelte:fragment slot="icon">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
@@ -96,14 +96,8 @@
 						><path
 							stroke-linecap="round"
 							stroke-linejoin="round"
-							d="M9 3.75H6.912a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859M12 3v8.25m0 0l-3-3m3 3l3-3"
+							d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z"
 						/></svg
-					>
-				</svelte:fragment>
-				<svelte:fragment slot="subtext">
-					<span
-						class="inline-flex justify-center items-center p-3 ml-3 w-3 h-3 text-sm font-medium text-blue-600 bg-blue-200 rounded-full dark:bg-blue-900 dark:text-blue-200"
-						>3</span
 					>
 				</svelte:fragment>
 			</SidebarItem>
